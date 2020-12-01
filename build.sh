02ad394ac49dececbfa0f5e6c1695b9311d1654b
ap-pauloafonso <ap.pauloafonso@gmail.com>
2020-11-30 21:05:36 -0300

    fix sh

--- build.sh
+++ build.sh
@@ -13,4 +13,5 @@ env GOOS=darwin GOARCH=amd64 go build -v -o ./out/mac/ratio-spoof github.com/ap-
 env GOOS=linux GOARCH=amd64 go build -v  -o ./out/linux/ratio-spoof github.com/ap-pauloafonso/ratio-spoof/cmd
 env GOOS=windows GOARCH=amd64 go build -v -o ./out/windows/ratio-spoof.exe github.com/ap-pauloafonso/ratio-spoof/cmd
 
-zip ./out/ratio-spoof-$version\(linux-mac-windows\).zip -r ./out
+cd out/
+zip ratio-spoof-$version\(linux-mac-windows\).zip -r .
